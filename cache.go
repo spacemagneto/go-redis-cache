80d96121e869a06657164f1efac4a6aa874b31d1
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-11-24 15:04:01 +0300

    base pkg struct

--- cache.go
+++ cache.go
@@ -1,1 +1,18 @@
 package cache
+
+import "github.com/redis/go-redis/v9"
+
+type RedisCache[T any] interface{}
+
+type Cache[T any] struct {
+	rdb        redis.UniversalClient
+	transcoder Transcoder[T]
+}
+
+func NewRedisCache[T any](client redis.UniversalClient) *Cache[T] {
+	return &Cache[T]{rdb: client, transcoder: NewPipelineTranscoder[T]()}
+}
+
+func NewRedisCacheWithTranscoder[T any](client redis.UniversalClient, transcoder Transcoder[T]) *Cache[T] {
+	return &Cache[T]{rdb: client, transcoder: transcoder}
+}
