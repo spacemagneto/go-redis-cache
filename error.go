06636efa5af7f45a4d7b7c81b7034c82541b4b12
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-11-28 17:50:22 +0300

    transcoder test cases

--- error.go
+++ error.go
@@ -1,1 +1,9 @@
 package cache
+
+import "errors"
+
+var (
+	ErrEmptyRedisClient = errors.New("redis client is empty")
+	ErrValueIsEmpty     = errors.New("value is empty")
+	ErrKeyIsEmpty       = errors.New("cache key is empty or contains whitespace")
+)
