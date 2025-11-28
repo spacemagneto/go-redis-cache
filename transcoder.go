296f783fb351a988fbaf707ea9f3711a010a8926
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-11-28 15:31:14 +0300

    default json transcoder

--- transcoder.go
+++ transcoder.go
@@ -1,7 +1,33 @@
 package cache
 
+import (
+	"github.com/goccy/go-json"
+)
+
 type Transcoder[T any] interface {
 	Encode(T) (string, error)
 
 	Decode(string) (T, error)
+}
+
+type defaultTranscoder[T any] struct{}
+
+func (defaultTranscoder[T]) Encode(src T) (string, error) {
+	var bytes []byte
+	var err error
+
+	bytes, err = json.Marshal(src)
+
+	return string(bytes), err
+}
+
+func (defaultTranscoder[T]) Decode(src string) (T, error) {
+	var entry T
+	var err error
+
+	if err = json.Unmarshal([]byte(src), &entry); err != nil {
+		return entry, err
+	}
+
+	return entry, nil
 }
